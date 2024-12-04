b6ca17d20916e254e5307c185cf4ae09621bd581
RafiBinWores <rafibinwores2203@gmail.com>
2024-12-04 16:11:12 +0600

    Product details page color not showing issue fixed

--- src/components/productDetails/ColorSelector.jsx
+++ src/components/productDetails/ColorSelector.jsx
@@ -6,11 +6,10 @@ function ColorSelector({colors}) {
     <>
       <p className="mt-5 text-sm text-neutral-400">Color</p>
       <div className="flex gap-4 mt-3">
-        {colors.map((color, index) => (
+        {colors.map((colors, index) => (
           <button
             key={index}
-            className={`flex shrink-0 ${color.color} h-[30px] rounded-[50px] w-[30px]`}
-            aria-label={`Select color ${index + 1}`}
+            className={`flex shrink-0 ${colors.color} h-[30px] rounded-[50px] w-[30px]`}
           />
         ))}
       </div>
